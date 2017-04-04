4a32d3f7a674e3515b1d364d6d3712bde76f5819
garon147@gmail.com <yftvybr222017yftvybR>
2017-04-04 23:34:16 +0300

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	public float speed;
+	public Boundary boundary;
+	public float tilt;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	private float nextFire;
+
+	void Update() {
+
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+
+			nextFire = Time.time + fireRate;
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+		}
+
+	}
+
+	void FixedUpdate() {
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		rb = GetComponent<Rigidbody> ();
+
+		Vector3 playerMovement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = playerMovement * speed;
+
+		Vector3 playerPosition = new Vector3 (
+			Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax),
+			0.0f,
+			Mathf.Clamp (rb.position.z, boundary.zMin, boundary.zMax)
+		);
+		rb.position = playerPosition;
+
+		rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * (- tilt));
+	}
+}
+
+[System.Serializable]
+public class Boundary {
+
+	public float xMin, xMax, zMin, zMax;
+}
