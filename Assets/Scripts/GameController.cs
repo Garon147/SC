d7e0aa3889d430a66e581a885ff1b006feddbc85
garon147@gmail.com <yftvybr222017yftvybR>
2017-04-06 01:18:49 +0300

    spawning waves created, boundary size changed

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,21 +6,32 @@ public class GameController : MonoBehaviour {
 
 	public GameObject hazard;
 	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
 
 	void Start() {
 
-		spawnWaves ();
+		StartCoroutine (spawnWaves ());
+
+
 	}
+		
+	IEnumerator spawnWaves() {
 
+		yield return new WaitForSeconds (startWait);
+		while (true) {
 
+			for (int i = 0; i < hazardCount; i++) {
 
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
 
-
-	void spawnWaves() {
-
-		Vector3 spawnPosition = new Vector3 (Random.Range( -spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-		Quaternion spawnRotation = Quaternion.identity;
-
-		Instantiate (hazard, spawnPosition, spawnRotation);
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+		}
 	}
 }
