6b14eba1157a7229480883d5241e688b21c420b2
garon147@gmail.com <yftvybr222017yftvybR>
2017-04-08 19:34:28 +0300

    different type of asteroids added, background scroll added, stars imitation added

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
-	public GameObject hazard;
+	public GameObject[] hazards;
 	public Vector3 spawnValues;
 	public int hazardCount;
 
@@ -38,7 +39,7 @@ public class GameController : MonoBehaviour {
 
 			if (Input.GetKeyDown (KeyCode.R)) {
 
-				Application.LoadLevel (Application.loadedLevel);
+				SceneManager.LoadScene ("Main");
 			}
 		}
 	}
@@ -49,6 +50,8 @@ public class GameController : MonoBehaviour {
 		while (true) {
 
 			for (int i = 0; i < hazardCount; i++) {
+
+				GameObject hazard = hazards [Random.Range (0, hazards.Length)];
 
 				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
 				Quaternion spawnRotation = Quaternion.identity;
