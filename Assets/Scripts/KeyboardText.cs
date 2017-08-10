d4c955cdffe8be2a09926f841e09d06570a9e806
Kirill Garon <garon147@gmail.com>
2017-08-10 23:41:34 +0300

    keyboard

---
+++ Assets/Scripts/KeyboardText.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class KeyboardText : MonoBehaviour 
+{
+	string word = null;
+	int wordIndex = -1;
+	string alpha = null;
+	string alpha2 = null;
+	public Text myName = null;
+	char[] nameChar = new char[30];
+
+	public void alphabetFunction(string alphabet)
+	{
+		wordIndex++;
+		char[] keepChar = alphabet.ToCharArray ();
+		nameChar [wordIndex] = keepChar [0];
+		alpha = nameChar [wordIndex].ToString ();
+		word += alpha;
+		myName.text = word;
+	}
+
+	public void backspace()
+	{
+		if (wordIndex >= 0) 
+		{
+			wordIndex--;
+			alpha2 = null;
+			for (int i = 0; i < wordIndex + 1; i++) 
+			{
+				alpha2 += nameChar [i].ToString ();
+			}
+			word = alpha2;
+			myName.text = word;
+		}
+	}
+
+	public void confirm()
+	{
+		SceneManager.LoadScene ("Main");
+	}
+}
